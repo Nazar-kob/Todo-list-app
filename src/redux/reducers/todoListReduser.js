8968e2a742685c20f9b911df1b24434beea6f4bd
nazarKobryn <kobrinnazar05@gmail.com>
2022-11-08 14:28:06 +0200

    redux toolkit init

--- src/redux/reducers/todoListReduser.js
+++ src/redux/reducers/todoListReduser.js
@@ -1,42 +1,61 @@
 import { v4 as uuidv4 } from 'uuid'
 
-const ADD_TODO = 'ADD_TODO';
-const DELETE_TODO = 'DELETE_TODO';
-const CHANGE_TODO = 'CHANGE_TODO';
-const CLEAR_TODO = 'CLEAR_TODO';
-const CLEAR_TODO_ISDONE = 'CLEAR_TODO_ISDONE';
+// was
+// const ADD_TODO = 'ADD_TODO';
+// const DELETE_TODO = 'DELETE_TODO';
+// const CHANGE_TODO = 'CHANGE_TODO';
+// const CLEAR_TODO = 'CLEAR_TODO';
+// const CLEAR_TODO_ISDONE = 'CLEAR_TODO_ISDONE';
 
 
-const addTodo = (text) => ({
-    type: ADD_TODO,
-    text: text,
-});
+// const addTodo = (text) => ({
+//     type: ADD_TODO,
+//     text: text,
+// });
 
-const deleteTodo = (idx) => ({ type: DELETE_TODO, id: idx });
+// const deleteTodo = (idx) => ({ type: DELETE_TODO, id: idx });
 
-const changeTodo = (idx) => ({ type: CHANGE_TODO, id: idx });
+// const changeTodo = (idx) => ({ type: CHANGE_TODO, id: idx });
 
-const clearTodo = () => ({ type: CLEAR_TODO });
+// const clearTodo = () => ({ type: CLEAR_TODO });
 
-const clearTodoIsDone = () => ({ type: CLEAR_TODO_ISDONE })
+// const clearTodoIsDone = () => ({ type: CLEAR_TODO_ISDONE })
 
-const todoReducer = (initialState = [], action) => {
-    switch (action.type) {
-        case ADD_TODO:
-            return [...initialState, { id: uuidv4(), text: action.text, isDone: false }];
-        case DELETE_TODO:
-            return [...initialState].filter(({ id }) => id !== action.id);
-        case CHANGE_TODO:
-            return [...initialState].map((obj) => obj.id === action.id ? { ...obj, isDone: !obj.isDone } : obj);
-        case CLEAR_TODO_ISDONE:
-            return [...initialState].filter(({ isDone }) => !isDone)
-        case CLEAR_TODO:
-            return [];
-        default:
-            return initialState;
+// const todoReducer = (initialState = [], action) => {
+//     switch (action.type) {
+//         case ADD_TODO:
+//             return [...initialState, { id: uuidv4(), text: action.text, isDone: false }];
+//         case DELETE_TODO:
+//             return [...initialState].filter(({ id }) => id !== action.id);
+//         case CHANGE_TODO:
+//             return [...initialState].map((obj) => obj.id === action.id ? { ...obj, isDone: !obj.isDone } : obj);
+//         case CLEAR_TODO_ISDONE:
+//             return [...initialState].filter(({ isDone }) => !isDone)
+//         case CLEAR_TODO:
+//             return [];
+//         default:
+//             return initialState;
+//     }
+// }
+
+// export const actions = { addTodo, deleteTodo, changeTodo, clearTodo, clearTodoIsDone };
+
+// export default todoReducer;
+
+import { createSlice } from '@reduxjs/toolkit'
+
+const startReduser = [];
+
+const todoListReduser = createSlice({
+    name: 'todo',
+    initialState: startReduser,
+    reducers: {
+        deleteTodo: (value, action) => value.filter(({ id }) => id !== action.payload),
+        changeTodo: (value, action) => value.map((obj) => (obj.id === action.payload ? { ...obj, isDone: !obj.isDone } : obj)),
+        addTodo: (value, action) => ([...value, { id: uuidv4(), text: action.payload, isDone: false }]),
+        clearTodo: () => [],
+        clearTodoIsDone: (value) => [...value].filter(({ isDone }) => !isDone)
     }
-}
-
-export const actions = { addTodo, deleteTodo, changeTodo, clearTodo, clearTodoIsDone };
-
-export default todoReducer;
+})
+export const { actions } = todoListReduser;
+export default todoListReduser.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,25 @@
-import { createStore, combineReducers } from 'redux'
+// import { createStore, combineReducers } from 'redux'
 import todoReducer from './reducers/todoListReduser'
 
+// was redux
 
-const redusers = combineReducers({
-    todo: todoReducer
-});
+// const redusers = combineReducers({
+//     todo: todoReducer
+// });
 
-const store = createStore(redusers);
+// const store = createStore(redusers);
+
+// export default store;
+
+// use redux toolkit
+import { configureStore } from '@reduxjs/toolkit';
+
+const store = configureStore({
+    reducer: {
+        todo: todoReducer
+    }
+})
 
 export default store;
+
+
